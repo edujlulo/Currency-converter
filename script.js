463f5f72a55ed1a537962c622f6771d4ac1566de
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-07-15 22:16:07 +0100

    Updates
    Updates

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const buttonCurrency1 = document.getElementById("button-currency1");
+const buttonCurrency2 = document.getElementById("button-currency2");
+const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 
 buttonCurrency1.addEventListener("click", function () {
-  input2.value = "hola mundo";
+  input1.value = parseFloat(
+    (input2.value * 0.8664760419374404).toFixed(2)
+  ).toLocaleString("en-UK");
+});
+
+buttonCurrency2.addEventListener("click", function () {
+  input2.value = parseFloat((input1.value * 1.1541).toFixed(2)).toLocaleString(
+    "en-UK"
+  );
 });
