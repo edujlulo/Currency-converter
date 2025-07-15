b2a9982bd10719abf917aec2fb8d7886397571c8
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-07-15 21:43:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const buttonCurrency1 = document.getElementById("button-currency1");
 const input2 = document.getElementById("input2");
+
+buttonCurrency1.addEventListener("click", function () {
+  input2.value = "hola mundo";
+});
